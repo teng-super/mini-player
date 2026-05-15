6fcbf83a5e8d2a57dea91171260e6ec1c0b64c21
Colin Scotten <zhangsiteng24@gmail.com>
2026-05-15 21:40:15 +0800

    feat:将demuxer类改为多线程版本

--- include/demuxer.h
+++ include/demuxer.h
@@ -1,10 +1,15 @@
 #pragma once
+#include <atomic>
 #include <string>
 #include <optional>
+#include <thread>
+#include <memory>
 #include "ffmpeg_raii.h"
+#include "bounded_blocking_queue.h"
 namespace mp{
     
 class Demuxer {
+    using PacketQueue = BoundedBlockingQueue<AVPacket*>;
     public:
         Demuxer() = default;
         ~Demuxer() = default;
@@ -12,8 +17,13 @@ class Demuxer {
         Demuxer(const Demuxer&) = delete;
         Demuxer& operator=(const Demuxer&) = delete;
 
-        bool Open(const std::string& path);//自定义函数，打开对应文件
-        bool ReadPacket(AVPacket* pkt);
+        bool Open(const std::string& path);//自定义函数，打开对应文件,成功返回true
+        void Start();//启动一个读包线程
+
+        void Stop();//停止此线程
+        //将原先的一次一次调用换成start,stop这样Run() + packet_queue()给代替
+
+        PacketQueue& packet_queue(){return packet_queue_;};
         int video_stream_index() const {return video_stream_idx_;} ;//返回对应流的下标
         AVCodecParameters* video_cpar() const;
         /*  codec_id       编码格式，比如 H.264 / H.265 / VP9
@@ -27,8 +37,13 @@ class Demuxer {
         //基于 container 和 codec 信息猜测帧率；第三个参数 frame 可以为 NULL；如果不知道帧率，会返回 0/1
         AVFormatContext* format_context() const {return fmt_ctx_.get();};//
     private:
+    void Run();//后台的跑函数
     int video_stream_idx_ = -1;
     AVFormatContextPtr fmt_ctx_;
+
+    PacketQueue packet_queue_{60}; // capacity 60的读packet队列
+    std::thread thread_;//读线程
+    std::atomic<bool> stop_requested_{false};//让线程开始和停止
 };
 
 }
